411afcc384c67b8aaa2ef9138ba20c4e1f939793
Anezka Klustova <ane.klustova.16@ucl.ac.uk>
2019-10-31 11:35:40 +0000

    changes

--- 05-testing/times.py
+++ 05-testing/times.py
@@ -1,15 +1,20 @@
 import datetime
 
 def time_range(t0, t1, n=1, g=0):
+    # n is number of steps
+    # g how many gaps in between observatios
+    # expect two elements of times
     t0_s = datetime.datetime.strptime(t0, "%Y-%m-%d %H:%M:%S")
     t1_s = datetime.datetime.strptime(t1, "%Y-%m-%d %H:%M:%S")
     d = (t1_s - t0_s).total_seconds() / n + g * (1/n - 1)
+    print(d)
     sec_range = [(t0_s + datetime.timedelta(seconds=i * d + i * g),
                   t0_s + datetime.timedelta(seconds=(i + 1) * d + i * g)) for i in range(n)]
     return [(ta.strftime("%Y-%m-%d %H:%M:%S"), tb.strftime("%Y-%m-%d %H:%M:%S")) for ta, tb in sec_range]
 
 
 def overlap_time(obs1, obs2):
+    # looking for overlap of observations
     ot = []
     for tr0, tr1 in obs1:
         for tra, trb in obs2:
@@ -17,6 +22,7 @@ def overlap_time(obs1, obs2):
             high = min(tr1, trb)
             ot.append((low, high))
     return ot
+    # gives list of time observations with a time where it started and finished 
 
 
 if __name__ == "__main__":
